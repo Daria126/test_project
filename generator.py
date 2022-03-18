3e921faafeb8b3094dbec27d317c6f8e7bf1b0ef
daria.sh <dariashyrchenko@gmail.com>
2022-03-18 04:50:30 -0700

    Adding numeration and second 2 task

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+# 1.
 def sq_gen():
     x = 1
     while True:
@@ -8,4 +9,18 @@ for n in sq_gen():
     if n > 1000:
         break
     print(n)
-    
+
+
+# 2. Pipeling
+def gen_int(n):
+    for i in range(n):
+        yield i
+
+def gen_2(gen):
+    print(gen)
+    for n in gen:
+        if n % 2:
+            yield n
+
+for i in gen_2(gen_int(10)):
+    print(i)
